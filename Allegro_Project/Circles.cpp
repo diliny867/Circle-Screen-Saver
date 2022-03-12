e9b71776a917e947cce199b773bd55493ea40ea9
diliny867 <diliny867@gmail.com>
2022-03-12 21:00:08 +0200

    fix. (not fixes)

--- Allegro_Project/Circles.cpp
+++ Allegro_Project/Circles.cpp
@@ -21,9 +21,9 @@ void Circles::CreateCircles(int n){
     }
 }
 void Circles::Move() {
-    double r;
-    double v;
+    double r,v;
     double tmp;
+    double v1, v2;
     for (int i = 0; i < circles.size(); i++) {
         v = circles[i]->GetV();
         circles[i]->IncCx(circles[i]->GetDx()*v);//set new position
@@ -70,26 +70,28 @@ void Circles::Move() {
                     circles[j]->SetDy(circles[i]->GetDy());
                     circles[i]->SetDy(tmp);
                     */
-                    tmp = circles[i]->GetDx();
-                    circles[i]->SetDx(circles[j]->GetDx() - tmp);
-                    circles[j]->SetDx(tmp - circles[j]->GetDx());
-                    tmp = circles[i]->GetDy();
-                    circles[i]->SetDy(circles[j]->GetDy() - tmp);
-                    circles[j]->SetDy(tmp - circles[j]->GetDy());
+                    v1 = circles[i]->GetV();
+                    v2 = circles[j]->GetV();
+                    tmp = circles[i]->GetDx()*v1;
+                    circles[i]->SetDx(circles[j]->GetDx()*v2 - tmp);
+                    circles[j]->SetDx(tmp - circles[j]->GetDx()*v2);
+                    tmp = circles[i]->GetDy()*v1;
+                    circles[i]->SetDy(circles[j]->GetDy()*v2 - tmp);
+                    circles[j]->SetDy(tmp - circles[j]->GetDy()*v2);
                     circles[i]->Normalise();
+                    circles[j]->Normalise();
                     circles[i]->IncCx(circles[i]->GetDx() * v);//set new position
                     circles[i]->IncCy(circles[i]->GetDy() * v);//set new position
                 }
             }
         }
-        
     }
-    
 }
 
 void Circles::Draw(){
     //ALLEGRO_FONT* font = al_load_ttf_font("Jaapokkienchance-Regular.otf", 10, NULL);
     for (int i = 0; i < circles.size(); i++) {
+        circles[i]->color = al_map_rgb(rand() % 256, rand() % 256, rand() % 256);
         al_draw_filled_circle(circles[i]->GetCx(),circles[i]->GetCy(),circles[i]->GetR(),circles[i]->color);
         //al_draw_multiline_textf(font, al_map_rgb(0, 255, 0), circles[i]->GetCx(), circles[i]->GetCy(), SCREEN_W, SCREEN_H, 1, "Cx: %f Cy: %f R: %f", circles[i]->GetCx(), circles[i]->GetCy(), circles[i]->GetR());
         //al_draw_multiline_textf(font, al_map_rgb(0, 255, 0), circles[i]->GetCx(), circles[i]->GetCy()-10, SCREEN_W, SCREEN_H, 1, "Dx: %f Dy: %f", circles[i]->GetDx(), circles[i]->GetDy());
